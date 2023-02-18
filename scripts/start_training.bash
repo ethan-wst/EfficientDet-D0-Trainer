d9d1c01327178afce09af407a09201371f46193d
Ethan West <108357494+ethan-wst@users.noreply.github.com>
2023-02-18 16:32:57 -0700

    Update start_training.bash

--- scripts/start_training.bash
+++ scripts/start_training.bash
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-cd ${PROJECT_ROOT}
-cd buff-code/src/efficientdet-d0-trainer/trainer/images/
-imagesDir=$(pwd)
+EFFDET_SRC="${PROJECT_ROOT}/src/efficientdet-d0-trainer"
+IMAGE_DIR="${EFFDET_SRC}/trainer/images"
+ANNO_DIR="${EFFDET_SRC}/trainer/annotations"
 
-cd ${PROJECT_ROOT}
-cd buff-code/src/efficientdet-d0-trainer/trainer/annotations/
-annoDir=$(pwd)
-
-cd ${PROJECT_ROOT}
-cd buff-code/src/efficientdet-d0-trainer/preprocessing/
-
-
-
-# Creates train.record
-python generate_tfrecord.py -x ${imagesDir}/train -l ${annoDir}/label_map.pbtxt -o ${imagesDir}/train.record
-
-# Creates test.record
-python generate_tfrecord.py -x ${imagesDir}/test -l ${annoDir}/label_map.pbtxt -o ${imagesDir}/test.record
-
-cd ${PROJECT_ROOT}
-cd buff-code/src/efficientdet-d0-trainer/trainer/
-
-#Begins training 
-python model_main_tf2.py --model_dir=models/my_ssd_effdet_d0 --pipeline_config_path=models/my_ssd_effdet_d0/pipeline.config
+# Creates .record files
+cd "${EFFDET_SRC}/preprocessing && \ 
+  python3 generate_tfrecord.py -x ${imagesDir}/train -l ${annoDir}/label_map.pbtxt -o ${imagesDir}/train.record && \
+  python3 generate_tfrecord.py -x ${imagesDir}/test -l ${annoDir}/label_map.pbtxt -o ${imagesDir}/test.record
+  
+# Begins model training 
+cd "${EFFDET_SRC}/trainer" && \
+  python3 model_main_tf2.py --model_dir=models/my_ssd_effdet_d0 --pipeline_config_path=models/my_ssd_effdet_d0/pipeline.config
+  
 echo -e "\n"
 echo "training should begin, after a little while and some warning messages a information print out with"
 echo "per-step time and loss statistics should appear, to monitor training using TensorBoard go to the"

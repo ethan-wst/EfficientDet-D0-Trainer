508dd3ef4a02ebf19aa38f376d1389b24a05413d
Ethan West <108357494+ethan-wst@users.noreply.github.com>
2023-02-18 17:05:23 -0700

    Create start_training.bash

--- scripts/start_training.bash
+++ scripts/start_training.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 EFFDET_SRC="${PROJECT_ROOT}/src/efficientdet-d0-trainer"
-IMAGE_DIR="${EFFDET_SRC}/trainer/images"
-ANNO_DIR="${EFFDET_SRC}/trainer/annotations"
+IMAGE_DIR="${PROJECT_ROOT}/src/${EFFDET_SRC}/trainer/images"
+ANNO_DIR="PROJECT_ROOT}/src/${EFFDET_SRC}/trainer/annotations"
 
 # Creates .record files
-cd "${EFFDET_SRC}/preprocessing" && \ 
-  python3 generate_tfrecord.py -x ${IMAGE_DIR}/train -l ${ANNO_DIR}/label_map.pbtxt -o ${IMAGE_DIR}/train.record && \
-  python3 generate_tfrecord.py -x ${IMAGE_DIR}/test -l ${ANNO_DIR}/label_map.pbtxt -o ${IMAGE_DIR}/test.record
+cd "${EFFDET_SRC}/preprocessing && \ 
+  python3 generate_tfrecord.py -x ${IMAGE_DIR}/train -l ${annoDir}/label_map.pbtxt -o ${IMAGE_DIR}/train.record && \
+  python3 generate_tfrecord.py -x ${IMAGE_DIR}/test -l ${annoDir}/label_map.pbtxt -o ${IMAGE_DIR}/test.record
   
 # Begins model training 
 cd "${EFFDET_SRC}/trainer" && \
@@ -17,6 +17,7 @@ echo -e "\n"
 echo "training should begin, after a little while and some warning messages a information print out with"
 echo "per-step time and loss statistics should appear, to monitor training using TensorBoard go to the"
 echo "github https://github.com/ethan-wst/efficientdet-d0-trainer.git and follow the instrucitons in the"
-echo "\'Monitor Training\' section"
+echo "\'Monito Training\' section"
 echo -e "\n"
+
 
